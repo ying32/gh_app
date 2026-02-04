52bcbb265fef2250f34c1487910733f7a8464274
不在乎y <ying_33@live.cn>
2026-02-04 23:10:22 +0800

    移除不要的依赖库，并在一些库上加上功能说明

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,6 @@ import 'dart:io';
 
 import 'package:fluent_ui/fluent_ui.dart' hide Page;
 import 'package:flutter/foundation.dart';
-import 'package:flutter_acrylic/flutter_acrylic.dart' as flutter_acrylic;
 import 'package:gh_app/utils/config.dart';
 import 'package:gh_app/utils/consts.dart';
 import 'package:gh_app/utils/github/github.dart';
@@ -31,60 +30,48 @@ void main() async {
     SystemTheme.accentColor.load();
   }
 
-  if (isDesktop) {
-    // 这个在macos下需要提前
-    // 为解决macos下启动时黑屏问题
-    // https://github.com/flutter/flutter/issues/142916
-    await WindowManager.instance.ensureInitialized();
-    if (defaultTargetPlatform == TargetPlatform.windows) {
-      // 这个只能windows下使用，否则会有问题
-      await flutter_acrylic.Window.initialize();
-      await flutter_acrylic.Window.hideWindowControls();
-    }
+  // 这个在macos下需要提前
+  // 为解决macos下启动时黑屏问题
+  // https://github.com/flutter/flutter/issues/142916
+  await WindowManager.instance.ensureInitialized();
+  final wSize =
+      Platform.isWindows ? const Size(1280, 768) : const Size(1000, 720);
 
-    final wSize =
-        Platform.isWindows ? const Size(1280, 768) : const Size(1000, 720);
-
-    /// 为解决启动时黑屏问题.还需要修改macos/Runner/MainFlutterWindow.swift里面的
-    /// https://github.com/flutter/flutter/issues/142916
-    ///```swift
-    ///class MainFlutterWindow: NSWindow {
-    ///   override func awakeFromNib() {
-    ///     // ...
-    ///     let flutterViewController = FlutterViewController()
-    ///     // Add following two lines
-    ///     self.backgroundColor = NSColor.clear
-    ///     flutterViewController.backgroundColor = NSColor.clear
-    ///     // ...
-    ///   }
-    /// }
-    ///```
-    if (Platform.isMacOS) {
-      await windowManager.ensureInitialized();
-      final option = WindowOptions(
-        center: true,
-        size: wSize,
-        minimumSize: wSize,
-        title: appTitle,
-      );
-      windowManager.waitUntilReadyToShow(option, () async {
-        await windowManager.show();
-        await windowManager.focus();
-      });
-    } else {
-      await windowManager.setTitle(appTitle);
-      await windowManager.setSize(wSize);
-      await windowManager.setMinimumSize(wSize);
-      await windowManager.center();
-      windowManager.waitUntilReadyToShow().then((_) async {
-        await windowManager.setTitleBarStyle(TitleBarStyle.hidden,
-            windowButtonVisibility: false);
-        await windowManager.show();
-        await windowManager.focus();
-        await windowManager.setPreventClose(true);
-        await windowManager.setSkipTaskbar(false);
-      });
-    }
+  /// 为解决启动时黑屏问题.还需要修改macos/Runner/MainFlutterWindow.swift里面的
+  /// https://github.com/flutter/flutter/issues/142916
+  ///```swift
+  ///class MainFlutterWindow: NSWindow {
+  ///   override func awakeFromNib() {
+  ///     // ...
+  ///     let flutterViewController = FlutterViewController()
+  ///     // Add following two lines
+  ///     self.backgroundColor = NSColor.clear
+  ///     flutterViewController.backgroundColor = NSColor.clear
+  ///     // ...
+  ///   }
+  /// }
+  ///```
+  if (Platform.isMacOS) {
+    await windowManager.ensureInitialized();
+    final option = WindowOptions(
+        center: true, size: wSize, minimumSize: wSize, title: appTitle);
+    windowManager.waitUntilReadyToShow(option, () async {
+      await windowManager.show();
+      await windowManager.focus();
+    });
+  } else {
+    await windowManager.setTitle(appTitle);
+    await windowManager.setSize(wSize);
+    await windowManager.setMinimumSize(wSize);
+    await windowManager.center();
+    windowManager.waitUntilReadyToShow().then((_) async {
+      await windowManager.setTitleBarStyle(TitleBarStyle.hidden,
+          windowButtonVisibility: false);
+      await windowManager.show();
+      await windowManager.focus();
+      await windowManager.setPreventClose(true);
+      await windowManager.setSkipTaskbar(false);
+    });
   }
 
   runApp(const GithubApp());
